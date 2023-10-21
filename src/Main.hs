0f2da71a3b3ea15775997614cbb1bc323dafa99b
stinderdaad <stin@hotmail.nl>
2023-10-21 13:45:03 +0200

    added some more functions and menu data type

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
-main :: IO()
-main = undefined
+import Graphics.Gloss.Interface.IO.Game
+import Controller
+import Model
+import View
+
+fps :: Int
+fps = 10
+
+main :: IO ()
+main = playIO (InWindow "Counter" (400, 400) (0, 0)) -- Or FullScreen
+              blue            -- Background color
+              fps               -- Frames per second
+              initState     -- Initial state
+              undefined--view             -- View function
+              undefined--input            -- Event function
+              undefined--step             -- Step function
--- src/Model.hs
+++ src/Model.hs
@@ -2,6 +2,9 @@ module Model where
 
 import Graphics.Gloss.Data.Vector
 import Graphics.Gloss.Data.Point
+import System.Random
+
+-- Data structures
 
 data GameState = GameState {
     player :: Player,
@@ -10,6 +13,8 @@ data GameState = GameState {
     time :: Float,
     settings :: Settings
 }
+
+data Menu = MainMenu | PauseMenu | GameOverMenu | HighScores
 
 data Object = PlayerObject Player | EnemyObject Enemy | BulletObject Bullet
 
@@ -22,7 +27,8 @@ data Direction = Vector Float Float
 data Player = Player {
     playerPosition :: Position,
     playerHealth :: Int,
-    playerAttack :: Attack
+    playerAttack :: Attack,
+    playerSpeed :: Float
 }
 
 data Enemy = Enemy {
@@ -30,12 +36,15 @@ data Enemy = Enemy {
     enemyHealth :: Int,
     enemySize :: (Int, Int),
     enemyAttack :: Attack,
+    enemySpeed :: Float,
     pointsWorth :: Int
 }
 
 data Bullet = Bullet {
     bulletPosition :: Position,
-    bulletDirection :: Direction
+    bulletDirection :: Direction,
+    bulletSpeed :: Float,
+    bulletSize :: (Int, Int)
 }
 
 data Item = Item {
@@ -52,6 +61,44 @@ data Settings = Settings {
     boss :: Enemy
 }
 
--- If i ever want to incorporate speed i can do that here
-newPosition :: Position -> Direction -> Position
-newPosition (Point x y) (Vector vx vy) = Point (x + vx) (y + vy)
+-- Functions
+
+initPlayer :: Player
+initPlayer = Player (Point 200 200) 3 Basic 2
+
+basicEnemy :: Enemy
+basicEnemy = Enemy (Point 400 200) 1 (10, 10) Basic 2 50
+
+toughEnemy :: Enemy
+toughEnemy = Enemy (Point 400 200) 3 (10, 10) Basic 2 100
+
+basicBoss :: Enemy
+basicBoss = Enemy (Point 400 200) 20 (50, 50) Basic 2 1000
+
+level1 :: Settings
+level1 = Settings 1 1 [basicEnemy, toughEnemy] basicBoss
+
+initState :: GameState
+initState = GameState {
+    player = initPlayer,
+    objects = [],
+    score = 0,
+    time = 100,
+    settings = level1
+}
+
+basicBullet :: Object -> Bullet
+basicBullet (PlayerObject player) = Bullet (playerPosition player) (Vector 1 0) 5 (2, 2)
+-- Direction should be towards players position, add later
+basicBullet (EnemyObject enemy) = Bullet (enemyPosition enemy) (Vector (-1) 0) 5 (2, 2)
+basicBullet _ = error "Cannot create bullet from bullet"
+
+bulletHitObject :: GameState -> Bullet -> Object -> GameState
+bulletHitObject gs bullet (PlayerObject player) = gs { player = player { playerHealth = playerHealth player - 1 } }
+
+playerDead :: GameState -> Bool
+playerDead gs = playerHealth (player gs) <= 0
+
+-- Direction should be normalized vector
+newPosition :: Position -> Direction -> Float -> Position
+newPosition (Point x y) (Vector vx vy) speed = Point (x + (vx * speed)) (y + (vy * speed))
